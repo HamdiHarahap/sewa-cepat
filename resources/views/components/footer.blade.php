f3bc2937c32deab6e87fb376df93917d706cf249
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-02-01 23:38:57 +0700

    commit

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,26 @@
-<div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
-</div>
+<footer class="bg-blue-800 px-44 py-12 text-white flex flex-col items-center gap-12">
+    <div class="flex justify-between items-start w-full">
+        <div class="flex flex-col gap-3">
+            <h2 class="text-xl font-bold">SewaCepat</h2>
+            <p>Jl. Medan Area Selatan Gg Mangkok <br> No 12, Medan Sumatera Utara</p>
+        </div>
+        <div class="flex flex-col gap-3">
+            <h3 class="font-semibold text-lg">Navigasi</h3>
+            <ul class="flex flex-col gap-1">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#tentang">Tentang</a></li>
+                <li><a href="#mobil">Mobil</a></li>
+                <li><a href="#kontak">Kontak</a></li>
+            </ul>
+        </div>
+        <div class="flex flex-col gap-3">
+            <h3 class="font-semibold text-lg">Kontak</h3>
+            <ul class="flex flex-col gap-1">
+                <li><span class="font-bold">Phone</span>: +6281260244154</li>
+                <li><span class="font-bold">Email</span>: sewacepat@gmail.com</li>
+            </ul>
+        </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.0443311920094!2d98.7001253749731!3d3.577285296396886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30313055a43efc61%3A0xd8271888f900de2d!2sGg.%20Mangkok%2C%20Sukaramai%20I%2C%20Kec.%20Medan%20Area%2C%20Kota%20Medan%2C%20Sumatera%20Utara%2020227!5e0!3m2!1sid!2sid!4v1738427779090!5m2!1sid!2sid" referrerpolicy="no-referrer-when-downgrade" class="rounded-md w-[30rem] h-[20rem]"></iframe>
+    </div>
+    <p>&copy; Copyright <b>SewaCepat</b>. All Right Reserved</p>
+</footer>
